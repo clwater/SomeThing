edcc09532e76802f34c1969ec91ab6514d1199f8
clwater <gengzhibo@gengzhibo.net>
2017-07-14 17:22:45 +0800

    20170714

--- Server/main.py
+++ Server/main.py
@@ -12,5 +12,5 @@ def make_app():
 
 if __name__ == "__main__":
     app = make_app()
-    app.listen(8888)
+    app.listen(9000)
     tornado.ioloop.IOLoop.current().start()
---
+++ Server/other.py
@@ -0,0 +1,8 @@
+import requests
+
+reponse = requests.get('http://vimeo.com')
+reponse = requests.get('https://www.google.com/')
+# reponse = requests.get('http://vimeo.com/channels/staffpicks/videos/page:1')
+html = reponse.text
+
+print html
