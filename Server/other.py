1aab21aa1473b41575d54c3d85aab23a68d42198
clwater <gengzhibo@gengzhibo.net>
2017-07-14 17:27:28 +0800

    20170714

--- Server/other.py
+++ Server/other.py
@@ -1,6 +1,6 @@
 import requests
 
-reponse = requests.get('http://vimeo.com')
+# reponse = requests.get('http://vimeo.com')
 reponse = requests.get('https://www.google.com/')
 # reponse = requests.get('http://vimeo.com/channels/staffpicks/videos/page:1')
 html = reponse.text
